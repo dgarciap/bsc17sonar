49452f47acc1efbf9b513990774aea57f82aefc5
David Garcia <david.garcia1@bsc.es>
2017-06-13 21:07:36 +0200

    Added visibility management according to camera position to tag component.

--- app/aframeComponents/tag.js
+++ app/aframeComponents/tag.js
@@ -1,3 +1,7 @@
+tag = {};
+
+tag.ACTIVATION_DISTANCE = 15;
+
 // Registering component
 AFRAME.registerComponent('tag', {
   schema: {
@@ -10,13 +14,25 @@ AFRAME.registerComponent('tag', {
       var sprite = new THREE.Sprite( spriteMaterial );
 
       this.el.setObject3D('sprite', sprite);
+      this.el.setAttribute('visible', false);
+  },
+
+  pythagorean: function(sideA, sideB){
+    return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
   },
 
   update: function () {
   },
 
   tick: function (time, timeDelta) {
-    
+    var camPos = document.querySelector('#app-camera').getAttribute('position');
+    var position = this.el.getAttribute('position');
+
+    var distance = this.pythagorean(position.x-camPos.x, position.z-camPos.z);
+
+    this.counter += timeDelta;
+    if(distance <= tag.ACTIVATION_DISTANCE) this.el.setAttribute('visible', true);
+    else this.el.setAttribute('visible', false);
   },
 
 });
